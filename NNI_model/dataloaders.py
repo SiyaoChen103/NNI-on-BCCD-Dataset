81cf74dbe2d4cb5a398506eb6db5ebbf33155723
SiyaoChen103 <114430519+SiyaoChen103@users.noreply.github.com>
2023-05-10 14:17:09 -0400

    Create dataloaders.py

---
+++ NNI_model/dataloaders.py
@@ -0,0 +1,6 @@
+import torch
+from torch.utils import data
+
+batch_size = 64
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
