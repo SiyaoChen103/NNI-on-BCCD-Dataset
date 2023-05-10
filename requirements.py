7771a624172731da14c0f40973fdfce6651007e9
SiyaoChen103 <114430519+SiyaoChen103@users.noreply.github.com>
2023-05-10 14:18:36 -0400

    Create requirements.py

---
+++ requirements.py
@@ -0,0 +1,15 @@
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import torch
+from torch.utils import data
+import numpy as np
+from torchvision import transforms
+import torchvision
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+import torch.nn as nn
+from torchvision import datasets, models, transforms
+import numpy as np 
+import pandas as pd 
+import warnings
+warnings.filterwarnings('ignore')
